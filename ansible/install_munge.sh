c2ad4ba6db5e8fb4e541318431484b4107a67ac4
Zongjun Hu <zongjunhu@yahoo.com>
2017-02-07 17:19:19 -0500

    made munge install/config work through ansible

---
+++ ansible/install_munge.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+cd /tmp
+unzip munge-munge-0.5.12.zip 
+cd munge-munge-0.5.12
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+make
+sudo make install
+sudo mv /usr/lib/libmung* /usr/lib64/
+sudo useradd -r -M munge
+sudo chown munge.munge /etc/munge
+sudo chown munge.munge /var/log/munge
+sudo chown munge.munge /var/run/munge
