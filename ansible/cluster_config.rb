782e42f769d7fae6e035fbec6423548fbab0e9ce
Zongjun Hu <zongjunhu@yahoo.com>
2017-02-08 00:39:31 -0500

    update vagrant to read cluster config file

---
+++ ansible/cluster_config.rb
@@ -0,0 +1,15 @@
+@slurm_cluster = {
+    :master=> {
+        :ipaddress => "10.10.10.1" 
+    },
+    :c01 => {
+        :ipaddress => "10.10.10.2" 
+    },
+    :c02 => {
+        :ipaddress => "10.10.10.3" 
+    },
+    :c03 => {
+        :ipaddress => "10.10.10.4" 
+    }
+}
+
