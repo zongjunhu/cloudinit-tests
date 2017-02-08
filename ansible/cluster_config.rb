34b6baa0016cf2cbf733a8b123187ed19c3cc5e3
Zongjun Hu <zongjunhu@yahoo.com>
2017-02-08 17:27:00 -0500

    update etc/hosts in shell privisioning

--- ansible/cluster_config.rb
+++ ansible/cluster_config.rb
@@ -1,15 +1,15 @@
 @slurm_cluster = {
     :master=> {
-        :ipaddress => "10.10.10.1" 
+        :ipaddress => "10.10.10.11" 
     },
     :c01 => {
-        :ipaddress => "10.10.10.2" 
+        :ipaddress => "10.10.10.12" 
     },
     :c02 => {
-        :ipaddress => "10.10.10.3" 
+        :ipaddress => "10.10.10.13" 
     },
     :c03 => {
-        :ipaddress => "10.10.10.4" 
+        :ipaddress => "10.10.10.14" 
     }
 }
 
